909dc2eb0c904989fdca7062a6f2ffba9d9730ff
brthai <brthai@ucsd.edu>
2017-12-08 11:29:15 -0800

    addplayer - Moved firebase code to js file, but doesn't seem to work due to import/export/module statement

--- public/addPlayer.js
+++ public/addPlayer.js
@@ -1,4 +1,39 @@
+import {auth, database} from './db.js';
 function add() {
+    var db = database;
+    var datab = db.ref('/team/players').push();
+    var key = datab.key;
+    
+    datab.set({
+        "fname": document.forms["newPlayer"]["fname"].value,
+        "lname": document.forms["newPlayer"]["lname"].value,
+        "profileImg": document.forms["newPlayer"]["profileImg"].value,
+        "email": document.forms["newPlayer"]["email"].value,
+        "dob": document.forms["newPlayer"]["dob"].value,
+        "jerseynumber": document.forms["newPlayer"]["jerseynumber"].value,
+        "position": document.forms["newPlayer"]["position"].value,
+        "captain": (document.forms["newPlayer"]["captain"].checked).toString(),
+        "starter": "false"
+    });
+
+    db.ref('/team/players/' + key + '/stats').set({
+        'assist': 0,
+        'goal': 0,
+        'shot': 0,
+        'onGoal': 0,
+        'foul': 0,
+        'red': 0,
+        'yellow': 0,
+        'corner': 0,
+        'gKick': 0,
+        'throw': 0,
+        'pen': 0
+    });
+
+
+
+    //Pre Firebase code
+/*  
     var team = JSON.parse(localStorage.getItem("team"));
     var edit = {};
     ["fname", "lname", "profileImg", "email", "dob", "jerseynumber", "position"].forEach(function(data){
@@ -11,4 +46,5 @@ function add() {
     edit["stats"] = {'assist': 0,'goal': 0,'shot': 0,'onGoal': 0,'foul': 0,'red': 0,'yellow': 0,'corner': 0,'gKick': 0,'throw': 0,'pen': 0}
     team.players.push(edit);
     localStorage.setItem("team", JSON.stringify(team));
+*/
 }
