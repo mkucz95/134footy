dc77d33ccca70b7ce81b0665437b9aa12756dee9
Mateusz Kucz <mati.kucz95@gmail.com>
2017-12-08 17:03:04 -0800

    home display

--- public/homeDisplay.js
+++ public/homeDisplay.js
@@ -3,42 +3,21 @@ import {auth, database} from './db.js';
 var team;
 
 function getTeamVal(){
-    console.log("getTeamVal");
-    database.ref('team/').once("value").then(s=>
-        {team = s.val();
-            console.log(team);
-            displayNextGame();
-            updateSeasonStats();
-        });
+    database.ref('team/').once("value").then(s=>{team = s.val();displayNextGame();updateSeasonStats(); });
 }
-
-var overallStats = {
-    'win':0,
-    'loss':0,
-    'tie':0,
-    'goalsFor':0,
-    'goalsAgainst':0
-}
-
+var overallStats = {'win':0,'loss':0,'tie':0,'goalsFor':0,'goalsAgainst':0}
 function updateSeasonStats(){
-    for(var i=0; i<team.schedule.length; i++){
-        if(team.schedule[i].type == "match"){
-        var goalFor = team.schedule[i].statsFor["goal"];
-        console.log(goalFor);
-        var goalAgainst = team.schedule[i].statsAgainst["goal"];
-        if(goalFor<goalAgainst){
-            overallStats['loss'] += 1;
-        }
-        else if(goalFor>goalAgainst){
-            overallStats['win'] += 1;
-        }
-        if(goalFor==goalAgainst){
-            overallStats['tie'] += 1;
-        }
-        overallStats['goalsFor'] += goalFor;
-        overallStats['goalsAgainst'] += goalAgainst;
-        }
-    }
+    team.schedule.forEach(event=>{
+        if(event.type == "match"){
+            var goalFor = parseInt(event.statsFor["goal"]);
+            var goalAgainst = parseInt(event.statsAgainst["goal"]);
+            if(goalFor<goalAgainst){overallStats['loss'] += 1;}
+            else if(goalFor>goalAgainst){overallStats['win'] += 1;}
+            if(goalFor==goalAgainst){overallStats['tie'] += 1;}
+            overallStats['goalsFor'] += goalFor;
+            overallStats['goalsAgainst'] += goalAgainst;
+            }
+    });
     ['win','loss','tie','goalsFor','goalsAgainst'].forEach(stat=>{
         let el = document.getElementById(stat);
         el.innerHTML = overallStats[stat];
@@ -76,7 +55,4 @@ function displayNextGame(){
         document.getElementById('extraInfo').style.visibility = 'hidden';
     }
 }
-window.onload = function(){
-    console.log("onload");
-    getTeamVal();
-}
+window.onload = getTeamVal;
