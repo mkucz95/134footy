3209020d8828ef2d0f40c5b54fa9ef468f8b9951
brthai <brthai@ucsd.edu>
2017-12-08 22:46:11 -0800

    Fixed dob reset

--- public/sw.js
+++ public/sw.js
@@ -69,28 +69,33 @@ self.addEventListener('activate', function (e) {
 });
 
 //Getting cache responses and returning them
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-      caches.match(e.request).then(function (response) {
-          if (response) {
-              return response;
-          }
-          var fetchReq = e.request.clone();
-          return fetch(fetchReq).then(
-              function(response){
-                  if(!response || response.status !== 200 || response.type !== 'basic') {
-                      return response;
-                  }
-                  var responseToCache = response.clone();
-                  caches.open(cacheID).then(function(cache){
-                      cache.put(e.request, responseToCache);
-                  });
-                  return response;
-              }
-          );
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request)
+      .then(function (response) {
+          return response || fetchAndCache(event.request);
       })
     );
 });
+
+function fetchAndCache(url) {
+    return fetch(url)
+    .then(function (response) {
+        // Check if we received a valid response
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return caches.open(CACHE_NAME)
+        .then(function (cache) {
+            cache.put(url, response.clone());
+            return response;
+        });
+    })
+    .catch(function (error) {
+        console.log('Request failed:', error);
+        // You could return a custom offline 404 page here
+    });
+}
 
 
 /*
