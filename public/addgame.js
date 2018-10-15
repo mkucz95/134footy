da7624d2c784d1723e4108e903608eb543db4cb5
Mati Kucz <mati.kucz95@gmail.com>
2018-10-15 15:46:35 -0700

    Update addgame.js

--- public/addgame.js
+++ public/addgame.js
@@ -67,37 +67,6 @@ function saveGame() {
 
     alert('Game Added.');
     window.location.href = "viewSchedule.html";
-
-
-
-
-    /*
-    var team = JSON.parse(localStorage.getItem("team"));
-    var newGame = {}
-    console.log(document.querySelector("#address"));    
-    ["opponent", "date", "time", "address"].forEach(info=>
-    newGame[info]=document.querySelector(#${info}).value);
-
-    newGame["statsFor"] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    newGame["statsAgainst"] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    var home_status = document.querySelector("#home").checked;
-    if (home_status){
-        newGame["location"] = "home";
-    }else{
-        newGame["location"] = "away";
-    }
-
-    var match_checked = document.querySelector("#match");
-    if (match_checked.checked) {
-        newGame["type"] = "match";
-    }else{
-        newGame["type"] = "practice";
-    }
-    team.schedule.push(newGame);
-    localStorage.setItem("team", JSON.stringify(team));
-
-    window.location = "viewSchedule.html";*/
 }
 
 window.onload=function(){
